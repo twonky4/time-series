779f95d7781e5025a8b1565b6d44c50edc35a6d6
Alexander Kose <twonky4@gmx.de>
2024-12-20 08:28:06 +0100

    setup maven

---
+++ src/main/java/de/viseit/TimeSeries.java
@@ -0,0 +1,4 @@
+package de.viseit;
+
+public final class TimeSeries {
+}
---
+++ src/test/java/de/viseit/TimeSeriesTest.java
@@ -0,0 +1,12 @@
+package de.viseit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class TimeSeriesTest {
+	@Test
+	void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
